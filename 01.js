4559fcba6d8c8914ee2b4d636faaede212abd4b8
Ijlal Furqaan <45873251+IjlalFurqaan@users.noreply.github.com>
2024-08-01 21:08:19 +0200

    .

---
+++ 01.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// hello.ts
+var message = "Hello, World!";
+console.log(message);
---
+++ 01.ts
@@ -0,0 +1,8 @@
+// hello.ts
+let message: string = "Hello, World!";
+console.log(message);
+
+let xyz =142;
+xyz.toFixed()
+
+export{}
